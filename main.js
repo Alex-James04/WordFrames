73ffbb0fbc4f06b1a8af1b1f24dc562ea36331fb
Alex James <Ajames04321@gmail.com>
2024-09-29 12:27:58 -0400

    Main Page Layout

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", () => {
     const menu = document.getElementById("Menu");
     const game = document.getElementById("Game");
-    const play_button = document.getElementById("Play Button");
+    const play_button = document.getElementById("PlayButton");
 
     play_button.addEventListener("click", () => {
         menu.style.display = "none";
